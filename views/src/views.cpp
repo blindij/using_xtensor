6f42e28729ffa686a32ca916e61e33120d89ed50
Bjorn Lindi <blindij@users.noreply.github.com>
2019-06-15 08:37:10 +0200

    Change output to be done by PRINTER/printer()
    The name of the template parameter is stringified and a part of the output. Recipe found on StackOverflow, see comment in file.

--- views/src/views.cpp
+++ views/src/views.cpp
@@ -6,24 +6,28 @@
 #include "xtensor/xview.hpp"
 #include "xtensor/xio.hpp"
 
+#define PRINTER(name) printer(#name, (name))
+// description of this construct founde here:
+// https://stackoverflow.com/questions/3386861/converting-a-variable-name-to-a-string-in-c
+
 template <typename T>
-void output_view(T V){
-    std::cout << "The view: " << std::endl;
+void printer(std::string const &name, T V){
+    std::cout << "The view " << name << ": " << std::endl;
     std::cout << V << std::endl;
     std::cout << std::endl;
 }
+
 int main(int argc, char *argv[]){
    std::vector<size_t> shape = {3, 2, 4};
    xt::xarray<int> a(shape);
 
    // View with same number of dimensions
    auto v1 = xt::view(a, xt::range(1, 3), xt::all(), xt::range(1, 3));
-   
-   std::cout << a << std::endl;
-   std::cout << v1 << std::endl;
 
-//   v1.shape()  = {2,2,2};
-   std::cout << v1(0,0,0) << std::endl; // a(1,0,1)
+   PRINTER(a);  // a is not view, but an array - wrong in output here.˚
+   PRINTER(v1);
+   //   v1.shape()  = {2,2,2};
+   PRINTER(v1(1,1,1));
    // => v(1,1,1) = a(2,1,2)
 
    // Generate a range and assign it to array of shape {3,2,4}
@@ -31,29 +35,27 @@ int main(int argc, char *argv[]){
    // Create view with the same number of dimensions
    auto v1b = xt::view(b, xt::range(1,3), xt::all(), xt::range(1, 3));
    // Write array and view to console
-   std::cout << b << std::endl;
-   std::cout << v1b << std::endl;
-   std::cout << std::endl;
+   PRINTER(b);
+   PRINTER(v1b);
    // Output v2(0,0,0) and v2(1,1,1)
-   std::cout << v1b(0,0,0) << std::endl;
-   std::cout << v1b(1,1,1) << std::endl;
-   std::cout << std::endl;
+   PRINTER(v1b(0,0,0));
+   PRINTER(v1b(1,1,1));
 
    // A view which reduce the number of dimensions
    auto v2 = xt::view(b, 1, xt::all(), xt::range(0, 4, 2));
    // v2.shape() = {2, 2}
    // v2(0,0)
-   output_view(v2);
-   output_view(v2(0,0));
+   PRINTER(v2);
+   PRINTER(v2(0,0));
 
    // A view increasing the number of dimensions
    auto v3 = xt::view(b,xt::all(), xt::all(), xt::newaxis(), xt::all());
    // v3.shape = { 3, 2, 1, 4 }
    // v3(0,0,0,0) = a(0,0,0)
 
-   output_view(v3(0,0,0,0));
-   output_view(v3(2,3,1,4));
-   output_view(v3);
+   PRINTER(v3(0,0,0,0));
+   PRINTER(v3(2,3,1,4));
+   PRINTER(v3);
 
     // A view with non-contiguous slices
     auto v4 = xt::view(b,xt::drop(0), xt::all(), xt::keep(0,3));
@@ -61,9 +63,9 @@ int main(int argc, char *argv[]){
     // => v4(0,0,0) = a(1,0,0)
     // => v4(1,1,1) = a(2,1,3)
 
-    output_view(v4(0,0,0));
-    output_view(v4(1,1,1));
-    output_view((v4));
+    PRINTER(v4(0,0,0));
+    PRINTER(v4(1,1,1));
+    PRINTER(v4);
 
 
 
