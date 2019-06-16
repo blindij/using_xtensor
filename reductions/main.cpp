796c47e81134f5bd85427eee59329fd28ec7718c
Bjorn Lindi <blindij@users.noreply.github.com>
2019-06-16 08:24:45 +0200

    Show example of xtensor xt::sum()
    More formatting of output.

--- reductions/main.cpp
+++ reductions/main.cpp
@@ -3,11 +3,20 @@
 #include <xtensor/xio.hpp>
 int main() {
     // Sum
+    std::cout << " ------- S u m ----------" << std::endl;
     xt::xarray<int> a_sum = {{1,2,3},{4,5,6}};
     xt::xarray<int> r0 = xt::sum(a_sum,{1});
+    xt::xarray<int> r1 = xt::sum(a_sum);
+    int r2 = xt::sum(a_sum)();
+    auto r3 = xt::sum(a_sum, {1});   // r3 is an unevaluated expression
     std::cout << "r0 = " << r0 << std::endl;
+    std::cout << "r1 = " << r1 << std::endl; // Outputs {21}, a 0D-tensor???
+    std::cout << "r2 = " << r2 << std::endl; // Outputs 21
+    std::cout << "r3 = " << r3 << std::endl; // Outputs {6,15}. Values are computed upon each access
+    std::cout << std::endl;
 
     // Prod
+    std::cout << " ------ P r o d u c t -------" << std::endl;
     xt::xarray<int> a_prod = {{1,2},{3, 4}};
     xt::xarray<int> p0 = xt::prod(a_prod, {1});
     xt::xarray<int> p1 = xt::prod(a_prod);
@@ -16,5 +25,7 @@ int main() {
     std::cout << "p1 = " << p1 << std::endl;
     std::cout << "p2 = " << p2 << std::endl;
     std::cout << "p3 = " << p3 << std::endl;
+    std::cout << std::endl;
+
     return 0;
 }
